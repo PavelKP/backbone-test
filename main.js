1688644cf369abaf315645e8061beb20ac254993
PavelKP <mr.pavel.krasnov@yandex.ru>
2021-03-08 23:43:17 +0300

    template: origin

--- main.js
+++ main.js
@@ -104,3 +104,16 @@ $(function () {
 });
 
 /* Templater */
+
+$(function() {
+	// Простой пример
+	var compiled = _.template("hello: <%= name %>");
+	$("#rocket").append(compiled({ name: "Moe" }));
+
+	// Из шаблона
+	var compiledFromTpl = _.template($("#rocketTpl").html()); // Конвертируем шаблон в HTML?
+	$("#rocket").append(compiledFromTpl({ name: "Bo" }));
+
+	// append - jquery
+	// Добавляет контент внутрь выбранных элементов в конец, после имеющихся
+})
