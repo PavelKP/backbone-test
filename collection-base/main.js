3e766f8ff86d000a713e359da6d8264c53c30e64
PavelKP <mr.pavel.krasnov@yandex.ru>
2021-03-15 00:15:38 +0300

    init collection

---
+++ collection-base/main.js
@@ -0,0 +1,37 @@
+var app = app || {};
+
+$(function () {
+
+  // Прототип моедли
+  var MyModel = Backbone.Model.extend({
+    defaults: {
+      size: 10,
+      weight: `100kg` 
+      // при добавлении новыйх моделей в коллекцию
+      // У них будут эти свойства, если не перезаписывать их
+    }
+  })
+
+  //Прототип коллекции
+  var MyCollection = Backbone.Collection.extend({
+    model: MyModel,
+  });
+
+  // Можно при инициализации сразу отправить массив или объект
+  var coll = new MyCollection();
+
+  var car = new MyModel({
+    size: 75
+  });
+
+  coll.add(car);
+
+  // пустой объект - создаётся модель по умолчанию
+  // Дополняется тем, что указано в фигурных скобках
+  coll.add({});
+
+  // Можем добавлять массив объектов
+  coll.add([ {size: 80}, {color: `white`}, {} ]); 
+
+  console.log(coll.toJSON());
+})
